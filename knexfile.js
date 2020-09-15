3c933cc3ae2d28775d848d6a99572830f2dc68a9
bekir16 <aslanbekirburak@gmail.com>
2020-09-15 23:58:35 +0300

    chore: change env

--- knexfile.js
+++ knexfile.js
@@ -3,7 +3,7 @@
 const databaseName = "postgres";
 const pg = require('pg');
 
-const connection_url = process.env.DATABASE_URL || `postgres://postgres:@localhost:5432/${databaseName}`;
+const connection_url = process.env.DATABASE_URL ||  `postgres://postgres:12345@34.65.65.53:5432/exam`//`postgres://postgres:@localhost:5432/${databaseName}`;
 
 module.exports = {
   client: 'pg',
